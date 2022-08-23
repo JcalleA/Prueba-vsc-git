07fbdb9e3bb53d9f042458ff194c1901a2e7b7be
JcalleA <kalle-1413@hotmail.com>
2022-08-23 08:22:19 -0500

    Insertar label

--- Vista/view.py
+++ Vista/view.py
@@ -10,6 +10,8 @@ ventana.title("Aseo acuavalle")
 buton1=ttk.Button(text="Aseo acuavalle")
 buton1.pack()
 
+label1=ttk.Label(text="Hola")
+label1.pack()
 menu=Menu(ventana)
 filemenu = Menu(menu)
 ventana.config(menu=menu)
