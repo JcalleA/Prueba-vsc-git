66cbe54da4ccd6e995f9aae8d5da111ac76e9497
JcalleA <kalle-1413@hotmail.com>
2022-08-23 07:47:04 -0500

    Insertar for

---
+++ CnsultarUsuarioDao.py
@@ -0,0 +1,26 @@
+import sqlite3
+import Utilidades.conexion
+
+
+
+def Consultar_Usuario(Nombre):
+    conn=Utilidades.conexion.create_connection(r'C:/Users/Calle1413/agua.db')
+    cursor = conn.cursor()
+    instruction="SELECT * FROM Cliente WHERE Nombre  LIKE '"+Nombre+"%'"
+    cursor.execute(instruction)
+    datos=cursor.fetchall()
+    conn.commit()
+    conn.close()
+    print(datos)
+    
+def Insertar_Medicion(Contador,Responsable,Lectura,Fecha):
+    conn=Utilidades.conexion.create_connection(r'C:/Users/Calle1413/agua.db')
+    cursor = conn.cursor()
+    instruction="INSERT INTO consumos  (ID_Contador,ID_Empleado, Lectura, Mes, Dia, Año)VALUES ("+Contador,Responsable,Lectura,Fecha+");"
+    cursor.execute(instruction)
+    datos=cursor.fetchall()
+    conn.commit()
+    conn.close()
+    print(datos)
+    
+Insertar_Medicion(1,1,800,'2022,08,05')
---
+++ Vista/view.py
@@ -0,0 +1,23 @@
+from struct import pack
+from tkinter import *
+from tkinter import ttk
+
+
+ventana = Tk()
+ventana.config(bd=50)
+ventana.title("Aseo acuavalle")
+
+buton1=ttk.Button(text="Aseo acuavalle")
+buton1.size = 10
+buton1.pack
+
+
+
+
+
+
+
+
+
+
+ventana.mainloop()
